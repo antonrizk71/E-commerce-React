58413d88e2424527179cded36c7147219217d738
anton <antonrizk71@gmail.com>
2025-09-16 13:56:22 +0300

    backend

--- src/components/Login/Signup.jsx
+++ src/components/Login/Signup.jsx
@@ -23,37 +23,37 @@ export default function Signup() {
     <div className="form">
       <h2>Sign Up</h2>
       <form onSubmit={handleSubmit}>
-        <input 
-          type="text" 
-          name="name" 
-          placeholder="Enter your name" 
-          onFocus={handleFocus} 
-          onBlur={handleBlur} 
-          required 
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter your name"
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          required
         />
-        <input 
-          type="email" 
-          name="email" 
-          placeholder="Enter your email" 
-          onFocus={handleFocus} 
-          onBlur={handleBlur} 
-          required 
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          required
         />
-        <input 
-          type="password" 
-          name="password" 
-          placeholder="Enter password" 
-          onFocus={handleFocus} 
-          onBlur={handleBlur} 
-          required 
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter password"
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          required
         />
-        <input 
-          type="password" 
-          name="confirmPassword" 
-          placeholder="Confirm password" 
-          onFocus={handleFocus} 
-          onBlur={handleBlur} 
-          required 
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm password"
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          required
         />
         <input type="submit" value="Sign Up" />
         <p>
